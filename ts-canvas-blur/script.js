a5b9f5cb83751de75bdcfc611bd06431cd2e0069
Ivan <ilushinvanya@gmail.com>
2021-11-14 00:18:44 +0300

    init

--- ts-canvas-blur/script.js
+++ ts-canvas-blur/script.js
@@ -1,22 +1,7 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var canvas;
 var c;
 // const circleCount = window.innerWidth / 9;
-var circleCount = 4;
+var circleCount = 1;
 var mouse = { x: null, y: null };
 var maxRadius = 200;
 // const maxRadius = window.innerWidth;
@@ -27,132 +12,66 @@ var Color = {
     }
 };
 var Circle = /** @class */ (function () {
-    function Circle(r_min, x, y, dx, dy, color) {
-        if (r_min === void 0) { r_min = randomNumber(maxRadius * 0.9, 20); }
-        if (x === void 0) { x = randomNumber(canvas.width, r_min); }
-        if (y === void 0) { y = randomNumber(canvas.height, r_min); }
+    function Circle(r_x_min, r_x, r_x_max, r_y_min, r_y, r_y_max, x, y, dx, dy, color) {
+        if (r_x_min === void 0) { r_x_min = randomNumber(maxRadius * 0.9, 20); }
+        if (r_x === void 0) { r_x = randomNumber(maxRadius * 0.9, 20); }
+        if (r_x_max === void 0) { r_x_max = randomNumber(maxRadius * 0.9, 20); }
+        if (r_y_min === void 0) { r_y_min = randomNumber(maxRadius * 0.9, 20); }
+        if (r_y === void 0) { r_y = randomNumber(maxRadius * 0.9, 20); }
+        if (r_y_max === void 0) { r_y_max = randomNumber(maxRadius * 0.9, 20); }
+        if (x === void 0) { x = randomNumber(canvas.width, r_x); }
+        if (y === void 0) { y = randomNumber(canvas.height, r_x); }
         if (dx === void 0) { dx = randomNumber(4, -2, [0]); }
         if (dy === void 0) { dy = randomNumber(4, -1, [0]); }
         if (color === void 0) { color = Color.getRandom(); }
-        this.r_min = r_min;
+        this.r_x_min = r_x_min;
+        this.r_x = r_x;
+        this.r_x_max = r_x_max;
+        this.r_y_min = r_y_min;
+        this.r_y = r_y;
+        this.r_y_max = r_y_max;
         this.x = x;
         this.y = y;
         this.dx = dx;
         this.dy = dy;
         this.color = color;
         this.draw();
-        this.r = r_min;
     }
-    Circle.prototype.side = function () {
-        return {
-            right: this.x + this.r,
-            left: this.x - this.r,
-            bottom: this.y + this.r,
-            top: this.y - this.r
-        };
-    };
     Circle.prototype.draw = function () {
-        // const innerRadius = 100, outerRadius = 200;
-        // const gradient = c.createRadialGradient(this.x, this.y, innerRadius, this.x, this.y, outerRadius);
-        // gradient.addColorStop(0, this.color);
-        // gradient.addColorStop(1, '#454698');
         c.beginPath();
-        // c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-        c.ellipse(this.x, this.y, this.r, maxRadius, Math.PI, 0, Math.PI * 2);
+        c.ellipse(this.x, this.y, this.r_x, this.r_y, Math.PI, 0, Math.PI * 2);
         c.fillStyle = this.color;
-        // c.fillStyle = gradient;
-        // c.filter = 'blur(60px)';
+        // c.filter = "blur(120px)"
         c.fill();
     };
     Circle.prototype.run = function () {
-        // detect collision
-        var sideRight = this.side().right > canvas.width;
-        var sideLeft = this.side().left < 0;
-        var sideBottom = this.side().bottom > canvas.height;
-        var sideTop = this.side().top < 0;
-        if (sideRight || sideLeft)
+        if (this.dx > 0 && this.r_x >= this.r_x_max)
             this.dx *= -1;
-        if (sideBottom || sideTop)
+        if (this.dx < 0 && this.r_x <= this.r_x_min)
+            this.dx *= -1;
+        if (this.dy > 0 && this.r_y >= this.r_y_max)
             this.dy *= -1;
-        // increase size
-        // if (
-        //     // (mouse.x != mouse.y) != 0 &&
-        //     this.side().left - mouse.x < 50 &&
-        //     mouse.x - this.side().right < 50 &&
-        //     this.side().top - mouse.y < 50 &&
-        //     mouse.y - this.side().bottom < 50 &&
-        //     this.r < maxRadius
-        // )
-        //     this.r += 3;
-        // else if (this.r > this.r_min) this.r -= 1;
+        if (this.dy < 0 && this.r_y <= this.r_y_min)
+            this.dy *= -1;
+        this.r_y += this.dy;
+        this.r_x += this.dx;
         // change position
-        this.x += this.dx;
-        this.y += this.dy;
+        // this.x += this.dx;
+        // this.y += this.dy;
         this.draw();
     };
     return Circle;
 }());
-var GreenCircle = /** @class */ (function (_super) {
-    __extends(GreenCircle, _super);
-    function GreenCircle() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    GreenCircle.prototype.run = function () {
-        var sideRight = this.side().right > canvas.width / 2;
-        var sideLeft = this.side().left < 0;
-        var sideBottom = this.side().bottom > canvas.height / 2;
-        var sideTop = this.side().top < 0;
-        if (sideRight || sideLeft)
-            this.dx *= -1;
-        if (sideBottom || sideTop)
-            this.dy *= -1;
-        // increase size
-        // if (
-        //     // (mouse.x != mouse.y) != 0 &&
-        //     this.side().left - mouse.x < 50 &&
-        //     mouse.x - this.side().right < 50 &&
-        //     this.side().top - mouse.y < 50 &&
-        //     mouse.y - this.side().bottom < 50 &&
-        //     this.r < maxRadius
-        // )
-        //     this.r += 3;
-        // else if (this.r > this.r_min) this.r -= 1;
-        // change position
-        this.color = Color.vector[0];
-        // this.x += this.dx;
-        // this.y += this.dy;
-        if (this.dx > 0) {
-            this.r += 1;
-            maxRadius += 1;
-        }
-        else {
-            this.r -= 1;
-            maxRadius -= 1;
-        }
-        _super.prototype.draw.call(this);
-    };
-    return GreenCircle;
-}(Circle));
-var Class = /** @class */ (function (_super) {
-    __extends(Class, _super);
-    function Class() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    return Class;
-}(Circle));
 var circles = [];
 function init() {
-    // setting up canvas
-    // document.body.innerHTML = "<canvas id='canvas'></canvas>";
     canvas = this.document.getElementById("canvas");
     c = canvas.getContext("2d");
     this.resetCanvas();
+    var greenCircle = new Circle(180, 200, 240, 600, 700, 1000, 0, canvas.height, 0.2, 0.3, Color.vector[0]);
+    var purpleCircle = new Circle(300, 350, 400, 300, 400, 500, 600, canvas.height, 0.2, 0.2, Color.vector[1]);
+    circles.push(greenCircle);
+    circles.push(purpleCircle);
     animation();
-    // adding circles
-    // for (let i = circleCount; i > 0; i--) {
-    //     circles.push(new Circle(200));
-    // }
-    circles.push(new GreenCircle(100, 150, 150));
 }
 function animation() {
     // clear canvas
@@ -185,4 +104,34 @@ window.addEventListener("mousemove", function (e) {
     mouse.y = e.clientY;
     //   console.log(mouse);
 });
+// run()
+// detect collision
+// const sideRight = this.side().right > canvas.width;
+// const sideLeft = this.side().left < 0;
+// const sideBottom = this.side().bottom > canvas.height;
+// const sideTop =  this.side().top < 0;
+//
+// if (sideRight || sideLeft)
+//     this.dx *= -1;
+// if (sideBottom || sideTop)
+//     this.dy *= -1;
+// increase size
+// if (
+//     // (mouse.x != mouse.y) != 0 &&
+//     this.side().left - mouse.x < 50 &&
+//     mouse.x - this.side().right < 50 &&
+//     this.side().top - mouse.y < 50 &&
+//     mouse.y - this.side().bottom < 50 &&
+//     this.r < maxRadius
+// )
+//     this.r += 3;
+// else if (this.r > this.r_min) this.r -= 1;
+// side() {
+//     return {
+//         right: this.x + this.r,
+//         left: this.x - this.r,
+//         bottom: this.y + this.r,
+//         top: this.y - this.r
+//     };
+// }
 //# sourceMappingURL=script.js.map
--- ts-canvas-blur/script.ts
+++ ts-canvas-blur/script.ts
@@ -1,7 +1,7 @@
 let canvas: HTMLCanvasElement;
 let c: CanvasRenderingContext2D;
 // const circleCount = window.innerWidth / 9;
-const circleCount = 4;
+const circleCount = 1;
 const mouse = { x: null, y: null };
 let maxRadius = 200;
 // const maxRadius = window.innerWidth;
@@ -17,137 +17,60 @@ class Circle {
     r;
 
     constructor(
-        public r_min = randomNumber(maxRadius * 0.9, 20),
-        public x = randomNumber(canvas.width, r_min),
-        public y = randomNumber(canvas.height, r_min),
+        public r_x_min = randomNumber(maxRadius * 0.9, 20),
+        public r_x = randomNumber(maxRadius * 0.9, 20),
+        public r_x_max = randomNumber(maxRadius * 0.9, 20),
+        public r_y_min = randomNumber(maxRadius * 0.9, 20),
+        public r_y = randomNumber(maxRadius * 0.9, 20),
+        public r_y_max = randomNumber(maxRadius * 0.9, 20),
+        public x = randomNumber(canvas.width, r_x),
+        public y = randomNumber(canvas.height, r_x),
         public dx = randomNumber(4, -2, [0]),
         public dy = randomNumber(4, -1, [0]),
         public color = Color.getRandom()
     ) {
         this.draw();
-        this.r = r_min;
-    }
-
-    side() {
-        return {
-            right: this.x + this.r,
-            left: this.x - this.r,
-            bottom: this.y + this.r,
-            top: this.y - this.r
-        };
     }
 
     draw() {
-        // const innerRadius = 100, outerRadius = 200;
-        // const gradient = c.createRadialGradient(this.x, this.y, innerRadius, this.x, this.y, outerRadius);
-        // gradient.addColorStop(0, this.color);
-        // gradient.addColorStop(1, '#454698');
         c.beginPath();
-        // c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-        c.ellipse(this.x, this.y, this.r, maxRadius, Math.PI, 0, Math.PI * 2);
+        c.ellipse(this.x, this.y, this.r_x, this.r_y, Math.PI, 0, Math.PI * 2);
         c.fillStyle = this.color;
-        // c.fillStyle = gradient;
-        // c.filter = 'blur(60px)';
+        // c.filter = "blur(120px)"
         c.fill();
     }
 
     run() {
-        // detect collision
-        const sideRight = this.side().right > canvas.width;
-        const sideLeft = this.side().left < 0;
-        const sideBottom = this.side().bottom > canvas.height;
-        const sideTop =  this.side().top < 0;
+        if(this.dx > 0 && this.r_x >= this.r_x_max) this.dx *= -1;
+        if(this.dx < 0 && this.r_x <= this.r_x_min) this.dx *= -1;
 
-        if (sideRight || sideLeft)
-            this.dx *= -1;
-        if (sideBottom || sideTop)
-            this.dy *= -1;
+        if(this.dy > 0 && this.r_y >= this.r_y_max) this.dy *= -1;
+        if(this.dy < 0 && this.r_y <= this.r_y_min) this.dy *= -1;
 
-
-        // increase size
-        // if (
-        //     // (mouse.x != mouse.y) != 0 &&
-        //     this.side().left - mouse.x < 50 &&
-        //     mouse.x - this.side().right < 50 &&
-        //     this.side().top - mouse.y < 50 &&
-        //     mouse.y - this.side().bottom < 50 &&
-        //     this.r < maxRadius
-        // )
-        //     this.r += 3;
-        // else if (this.r > this.r_min) this.r -= 1;
+        this.r_y += this.dy;
+        this.r_x += this.dx;
 
         // change position
-        this.x += this.dx;
-        this.y += this.dy;
+        // this.x += this.dx;
+        // this.y += this.dy;
 
         this.draw();
     }
 }
 
-class GreenCircle extends Circle {
-    run() {
-
-        const sideRight = this.side().right > canvas.width / 2;
-        const sideLeft = this.side().left < 0;
-        const sideBottom = this.side().bottom > canvas.height / 2;
-        const sideTop =  this.side().top < 0;
-
-        if (sideRight || sideLeft)
-            this.dx *= -1;
-        if (sideBottom || sideTop)
-            this.dy *= -1;
-
-
-        // increase size
-        // if (
-        //     // (mouse.x != mouse.y) != 0 &&
-        //     this.side().left - mouse.x < 50 &&
-        //     mouse.x - this.side().right < 50 &&
-        //     this.side().top - mouse.y < 50 &&
-        //     mouse.y - this.side().bottom < 50 &&
-        //     this.r < maxRadius
-        // )
-        //     this.r += 3;
-        // else if (this.r > this.r_min) this.r -= 1;
-
-        // change position
-
-        this.color = Color.vector[0];
-        // this.x += this.dx;
-        // this.y += this.dy;
-        if(this.dx > 0) {
-            this.r += 1
-            maxRadius += 1
-        } else {
-            this.r -= 1
-            maxRadius -= 1
-        }
-
-
-        super.draw();
-    }
-}
-
-class Class extends Circle {
-
-}
-
-
 let circles: Circle[] = [];
 
 function init() {
-    // setting up canvas
-    // document.body.innerHTML = "<canvas id='canvas'></canvas>";
     canvas = this.document.getElementById("canvas");
     c = canvas.getContext("2d");
     this.resetCanvas();
-    animation();
 
-    // adding circles
-    // for (let i = circleCount; i > 0; i--) {
-    //     circles.push(new Circle(200));
-    // }
-    circles.push(new GreenCircle(100,150,150));
+    const greenCircle = new Circle(180, 200, 240, 600, 700, 1000, 0, canvas.height, 0.2, 0.3, Color.vector[0])
+    const purpleCircle = new Circle(300, 350, 400, 300, 400, 500, 600, canvas.height, 0.2, 0.2, Color.vector[1])
+    circles.push(greenCircle);
+    circles.push(purpleCircle);
+
+    animation();
 }
 
 function animation() {
@@ -185,3 +108,38 @@ window.addEventListener("mousemove", function (e) {
     mouse.y = e.clientY;
     //   console.log(mouse);
 });
+
+
+// run()
+// detect collision
+// const sideRight = this.side().right > canvas.width;
+// const sideLeft = this.side().left < 0;
+// const sideBottom = this.side().bottom > canvas.height;
+// const sideTop =  this.side().top < 0;
+//
+// if (sideRight || sideLeft)
+//     this.dx *= -1;
+// if (sideBottom || sideTop)
+//     this.dy *= -1;
+
+
+// increase size
+// if (
+//     // (mouse.x != mouse.y) != 0 &&
+//     this.side().left - mouse.x < 50 &&
+//     mouse.x - this.side().right < 50 &&
+//     this.side().top - mouse.y < 50 &&
+//     mouse.y - this.side().bottom < 50 &&
+//     this.r < maxRadius
+// )
+//     this.r += 3;
+// else if (this.r > this.r_min) this.r -= 1;
+
+// side() {
+//     return {
+//         right: this.x + this.r,
+//         left: this.x - this.r,
+//         bottom: this.y + this.r,
+//         top: this.y - this.r
+//     };
+// }
